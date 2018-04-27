cb2e891bd27759f14d5e38af4ea085ee961786d0
Tyler Freedman <mail@tylerfreedman.com>
2018-04-27 11:16:42 -0400

    First version with support for reading Android SMS Backup & Restore files for initial import

--- maxs.rb
+++ maxs.rb
@@ -1,10 +1,7 @@
-require 'base64'
-require 'sinatra'
-require 'httparty'
-require 'json'
-require 'yaml'
-require 'phonelib'
-require 'time'
+require 'bundler/setup'
+Bundler.require(:default)
+
+require 'active_record'
 
 set :bind, '0.0.0.0'
 set :port, 8087
@@ -14,6 +11,28 @@ $config = JSON.parse(file)
 $config = $config.merge(YAML.load(File.read('maxs-registration.yaml')))
 Phonelib.default_country = $config["defaultCountry"]
 $sync_since = nil
+
+ActiveRecord::Base.logger = Logger.new(STDERR)
+
+ActiveRecord::Base.establish_connection(
+    adapter: "sqlite3",
+    database: 'sms.db'
+ )
+
+class Sms < ActiveRecord::Base
+end
+
+if !Sms.table_exists?
+  ActiveRecord::Schema.define do
+    create_table :sms do |table|
+      table.column :time, :integer
+      table.column :sender, :text
+      table.column :content, :text
+      table.column :room, :text
+      table.column :parsed, :boolean
+    end
+  end
+end
 
 def matrix_send(message, room, user, timestamp = Time.now.to_i * 1000)
   if room.start_with?('#')
@@ -96,16 +115,54 @@ def message_exists?(message)
 
   exists = false
   search_results["search_categories"]["room_events"]["results"].each do |r|
-    if (message[:time] - r["result"]["origin_server_ts"]).abs <= 1000 && message[:content] == r["result"]["content"]["body"] && r["result"]["sender"] == "@phone_#{message[:sender].gsub(' ', '').gsub('+', '=')}:#{$config['bridge']['domain']}" && room_id == r["result"]["room_id"]
+    if message[:time] == r["result"]["origin_server_ts"] && message[:content] == r["result"]["content"]["body"] && r["result"]["sender"] == "@phone_#{message[:sender].gsub(' ', '').gsub('+', '=')}:#{$config['bridge']['domain']}" && room_id == r["result"]["room_id"]
       exists = true
       break
     end
   end
+
+  if !exists
+    puts "NO MATCH FOUND - LISTING ALL CANDIDATES (#{search_results['search_categories']['room_events']['results'].count})"
+    if search_results['search_categories']['room_events']['results'].count == 0
+      puts "NO SEARCH RESULTS - DUMPING SEARCH JSON FOR INSPECTION"
+      puts search_json.inspect
+    end
+    search_results["search_categories"]["room_events"]["results"].each do |r|
+      puts r.inspect
+    end
+  end
+
   return exists
 end
 
 get '/' do
-  '{}'
+
+end
+
+get '/import' do
+  content_type :json
+
+  ActiveRecord::Base.logger = nil # Disable logging for speed, since this page is likely to generate a few zillion SQL queries.
+  Sms.find_each do |sms|
+    sms.destroy
+  end
+
+  doc = File.open("sms.xml") { |f| Nokogiri::XML(f) }
+  doc.css("smses sms").each do |sms|
+    sender = sms["address"]
+    if Phonelib.parse(sender).valid?
+      sender = Phonelib.parse(sms["address"]).international
+    end
+    room = sender.gsub(' ', '').gsub('+', '=')
+    if sms["type"].to_i == 2
+      sender = $config['puppet']['id']
+    end
+    if Sms.where(time: sms["date"].to_i, content: sms["body"], sender: sender, room: room).first.nil?
+      Sms.create(time: sms["date"].to_i, content: sms["body"], sender: sender, parsed: false, room: room)
+    end
+  end
+
+  Sms.all.to_json
 end
 
 get '/register' do
@@ -144,17 +201,14 @@ put '/transactions/:txn_id' do
   json["events"].each do |event|
     room_id = event["room_id"]
     room_alias = JSON.parse(HTTParty.get("#{$config['bridge']['homeserverUrl']}/_matrix/client/r0/rooms/#{room_id}/state/m.room.canonical_alias?access_token=#{$config['as_token']}").body)["alias"]
-    puts room_alias
     if room_alias && room_alias.start_with?('#phone_') && room_alias.end_with?(':' + $config["bridge"]["domain"]) && event["content"]["msgtype"] == 'm.text' && event["sender"] == $config["puppet"]["id"] && !event["content"]["body"].end_with?("\ufeff")
       phone_number = room_alias.split(':')[0].split('#phone_')[1].gsub('=', '+')
-      puts "PHONE_NUMBER: #{phone_number}"
       text = event["content"]["body"]
       maxs_send("sms send #{phone_number}  #{text}")
     elsif event["sender"] == "@phone:#{$config['bridge']['domain']}" && event["content"]["msgtype"] == 'm.text'
       body = event["content"]["body"]
       if body.include?("New SMS Received\n")
         message = parse_sms(body.split("New SMS Received\n")[1])
-        puts message.inspect
         if message[:sender] != $config['puppet']['id']
           matrix_send(message[:content], "#phone_#{message[:room]}:#{$config['bridge']['domain']}", "@phone_#{message[:room].gsub('+', '=')}:#{$config['bridge']['domain']}", message[:time])
         end
@@ -173,13 +227,11 @@ put '/transactions/:txn_id' do
         body = body.split("\n", 2)[1]
         messages = body.scan(/\n(From|To)\s+(.*?)(?=(?=\n(?=From|To)|$))/)
         messages.each do |m|
-          if m[0] == "From"
-            message = parse_sms("#{m[0]} #{m[1]}")
-            puts "#{message.inspect} - #{message_exists?(message)}"
-            if !message_exists?(message)
-              if message[:sender] != $config['puppet']['id']
-                matrix_send(message[:content], "#phone_#{message[:room]}:#{$config['bridge']['domain']}", "@phone_#{message[:room].gsub('+', '=')}:#{$config['bridge']['domain']}", message[:time]) #Backfill it!
-              end
+          message = parse_sms("#{m[0]} #{m[1]}")
+          puts "==> #{message.inspect}"
+          if !message_exists?(message)
+            if message[:sender] != $config['puppet']['id']
+#              matrix_send(message[:content], "#phone_#{message[:room]}:#{$config['bridge']['domain']}", "@phone_#{message[:room].gsub('+', '=')}:#{$config['bridge']['domain']}", message[:time]) #Backfill it!
             end
           end
         end
